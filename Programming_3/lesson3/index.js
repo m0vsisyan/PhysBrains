ab760968e84fb8e937462eb2db073e26dbe78d16
m0vsisyan <khachik.m3.y@tumo.org>
2019-06-05 18:23:11 +0400

    Homeworkes done

--- Programming_3/lesson3/index.js
+++ Programming_3/lesson3/index.js
@@ -16,7 +16,17 @@ app.get("/name/:name", function (req, res) {
     var name = req.params.name;
     res.send("<h1>Hello " + name + "</h1>");
 });
+// H O M E W O R K E S
+// 1
+app.get("/google/", function (req, res) {
+    res.redirect('http://google.com');
+});
 
+//2
+app.get("/google/:search", function (req, res) {
+    var search = req.params.search;
+    res.redirect('http://google.com/search?q='+ search);
+});
 app.listen(3000, function () {
     console.log("Example is running on port 3000");
 });
