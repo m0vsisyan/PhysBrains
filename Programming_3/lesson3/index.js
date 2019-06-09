63daea60964e2957e643ab0c4f3f50641cbaeed6
Your Name <you@example.com>
2019-06-09 16:10:11 +0400

    +lessson4

--- Programming_3/lesson3/index.js
+++ Programming_3/lesson3/index.js
@@ -1,13 +1,6 @@
 var express = require("express");
 var app = express();
 
-// app.get("/", function(req, res){
-//     res.send("Hello world");
-// });
-
-// app.listen(3000, function() {
-//     console.log("Example is running on port 3000");
-// }); 
 app.get("/", function (req, res) {
     res.send("<h1>Hello world</h1>");
 });
