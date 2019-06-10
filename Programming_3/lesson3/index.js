b43eae4be485d3fa9264712a9313cd184ebe0356
m0vsisyan <khachik.m3.y@tumo.org>
2019-06-10 16:20:18 +0400

    +modules

--- Programming_3/lesson3/index.js
+++ Programming_3/lesson3/index.js
@@ -1,25 +1,29 @@
 var express = require("express");
 var app = express();
 
-app.get("/", function (req, res) {
-    res.send("<h1>Hello world</h1>");
-});
+// app.get("/", function(req, res){
+//     res.send("Hello world");
+// });
 
-app.get("/name/:name", function (req, res) {
-    var name = req.params.name;
-    res.send("<h1>Hello " + name + "</h1>");
-});
-// H O M E W O R K E S
-// 1
-app.get("/google/", function (req, res) {
-    res.redirect('http://google.com');
-});
+// app.listen(3000, function() {
+//     console.log("Example is running on port 3000");
+// }); 
+// app.get("/", function (req, res) {
+//     res.send("<h1>Hello world</h1>");
+// });
 
-//2
-app.get("/google/:search", function (req, res) {
-    var search = req.params.search;
-    res.redirect('http://google.com/search?q='+ search);
-});
-app.listen(3000, function () {
-    console.log("Example is running on port 3000");
-});
+// app.get("/name/:name", function (req, res) {
+//     var name = req.params.name;
+//     res.send("<h1>Hello " + name + "</h1>");
+// });
+// // H O M E W O R K E S
+// // 1
+// app.get("/google/", function (req, res) {
+//     res.redirect('http://google.com');
+// });
+
+// //2
+// app.get("/google/:search", function (req, res) {
+//     var search = req.params.search;
+//     res.redirect('http://google.com/search?q='+ search);
+// });
